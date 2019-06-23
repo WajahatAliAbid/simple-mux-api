212f0c8035f050ac1d6db9357667dbffd0615157
WajahatAliAbid <wajahatckl@outlook.com>
2019-06-23 23:04:01 +0500

    made person struct in main.go

--- Person.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type Person struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-}
--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Person struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
 var people []Person
 
 // Get all people
@@ -16,19 +22,27 @@ func getPeople(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 
+// Get required person
 func getRequiredPerson(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(people)
 }
 
+// Create a new person
 func createPerson(w http.ResponseWriter, r *http.Request) {
 }
+
+// Update an existing person
 func updatePerson(w http.ResponseWriter, r *http.Request) {
 }
+
+// Delete a person
 func deletePerson(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	people = append(people, Person{1, "Wajahat", "Ali"})
+	people = append(people, Person{2, "Ali", "Iftikhar"})
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", getPeople).Methods("GET")
@@ -36,8 +50,8 @@ func main() {
 	router.HandleFunc("/", createPerson).Methods("POST")
 	router.HandleFunc("/{id}", updatePerson).Methods("PUT")
 	router.HandleFunc("/{id}", deletePerson).Methods("DELETE")
-	log.Println("Starting server at 3000")
 
+	log.Println("Starting server at 3000")
 	output := http.ListenAndServe(":3000", router)
 	log.Fatal(output)
 }
